285516bbc85abf91bb7c989e89f61f55e0c6f75f
Nafiu Taiwo <69268373+taiwonaf@users.noreply.github.com>
2023-02-17 13:08:15 +0100

    Inital commit
    - installed create react app
    - installed and setup tailwindcss
    - removed create react app templates

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-[40px] text-black">Movie web app</h1>
+    </div>
+  );
+}
+
+export default App;
